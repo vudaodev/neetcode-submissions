aefbdcf338cdfb1d5db9f1c1a88cdee951ecde28
Vu Dao <112084149+vudaodev@users.noreply.github.com>
2026-07-13 15:21:43 +0100

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(node):
+            if not node: return [True, 0]
+
+            left, right = dfs(node.left), dfs(node.right)
+            # Balanced if:
+            # 1. Left subtree is balanced 
+            # 2. Right subtree is balanced 
+            # 3. Balanced from the root 
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            # Return [whether tree is balanced, max height + 1]
+            # We add one to take into account the current node too
+            return [balanced, max(left[1],right[1]) + 1] 
+
+        return dfs(root)[0]
