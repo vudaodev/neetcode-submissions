23c57467da1e2c92ef3f9648088d352251a087fe
Vu Dao <112084149+vudaodev@users.noreply.github.com>
2026-07-15 14:13:57 +0100

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.py
@@ -0,0 +1,35 @@
+'''
+Brute force solution is O(n^3)
+We sort the array O(nlogn), so it's easier to work with.
+
+For each value at i, we use two pointers with j and k to see if we can find two values
+that add up to a target: -nums[i]
+
+If nums[j] + nums[k] == target, add to res
+If nums[j] + nums[k] > target, decrement k as we need smaller number.
+If nums[j] + nums[k] < target, increment k.
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        res = []
+        for i in range(len(nums)-2):
+            # We need a way of skipping duplicate values
+            # if i > 0 and nums[i] == nums[i-1]:
+            #     continue
+            target = 0 - nums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                jk_sum = nums[j] + nums[k]
+                if jk_sum == target:
+                    triplet = [nums[i],nums[j],nums[k]]
+                    if triplet not in res:
+                        res.append(triplet)
+                        # We need a way of skipping duplicate values
+                    else: j += 1
+                elif jk_sum > target:
+                    k -= 1
+                else: 
+                    j += 1  
+        return res
