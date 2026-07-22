e2c8d80fe6592e9c4acc86fa5f05c9fd973e8422
Vu Dao <112084149+vudaodev@users.noreply.github.com>
2026-07-22 17:20:43 +0100

    Add: find-pivot-index - submission-0

---
+++ Data Structures & Algorithms/find-pivot-index/submission-0.py
@@ -0,0 +1,27 @@
+'''
+- Find the sum in array from index 1 to end
+  -> This will be our starting rightSum
+- Start pointer at index 0, and leftSum at 0
+- while pointer < len(nums):
+    - Compare leftSum and rightSum
+        - if equal, return index
+        - if not equal:
+            add to leftSum
+            remove from rightSum
+            move pointer
+    [1,7,3,6,5,6]
+     ^            leftSum = 0, rightSum = 27
+       ^          leftSum += 1 = 1, rightSum -= 7 = 20
+'''
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        rightSum = sum(nums)
+        i, leftSum = 0,0
+        while i < len(nums):
+            rightSum -= nums[i]
+            if leftSum == rightSum:
+                return i
+            leftSum += nums[i]
+            i += 1
+        
+        return -1
